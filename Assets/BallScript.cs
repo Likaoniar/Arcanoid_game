c3eb38cced58e8a0f6a9749249e04e171a20faf4
Likaoniar <likaoniar@gmail.com>
2019-06-05 23:24:43 +0200

    ściany + piłka

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -6,6 +6,9 @@ public class BallScript : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,23 @@ public class BallScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!inPlay) // ! jest użyty jako skrót
+        {
+            transform.position = paddle.position;
+
+        }
         
     }
+
+    //pilka a solna granica
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("bottom"))
+        {
+            Debug.Log("Ball hit the bottom of the screen");
+        }
+    }
+
+
+
 }
